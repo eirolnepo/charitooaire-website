cabcb26bb624c7907ec8bbea0331a2e3cbf122c3
eirol <eiroljohn35@gmail.com>
2024-02-16 17:16:56 +0800

    added an image carousel in about us page, removed sign in modal

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+let slideIndex = 0; // Start at index 0
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n === undefined) { n = ++slideIndex; } // Increment slideIndex if n is not defined
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
