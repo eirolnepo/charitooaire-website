a24ad654a923d8142e2e172fe89d9c3bcd2d37db
eirol <eiroljohn35@gmail.com>
2024-02-18 02:08:24 +0800

    added modals on about us page

--- abt-us-script.js
+++ abt-us-script.js
@@ -27,3 +27,37 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+function openNewModal() {
+  document.getElementById('sign-in-modal').style.display='none';
+  document.getElementById('not-customer-modal').style.display='block';
+}
+
+function goBack() {
+  document.getElementById('not-customer-modal').style.display='none';
+  document.getElementById('sign-in-modal').style.display='block';
+}
+
+function closeNewModal() {
+  document.getElementById('not-customer-modal').style.display='none';
+}
+
+function openAdminModal() {
+  document.getElementById('not-customer-modal').style.display='none';
+  document.getElementById('admin-modal').style.display='block';
+}
+
+function goBackAdmin() {
+  document.getElementById('admin-modal').style.display='none';
+  document.getElementById('not-customer-modal').style.display='block';
+}
+
+function openEmployeeModal() {
+  document.getElementById('not-customer-modal').style.display='none';
+  document.getElementById('employee-modal').style.display='block';
+}
+
+function goBackEmployee() {
+  document.getElementById('employee-modal').style.display='none';
+  document.getElementById('not-customer-modal').style.display='block';
+}
