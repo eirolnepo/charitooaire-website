98ec80136c1564ae547bab72354fb16edb03a61a
mordredss <lawrencemilitante5@gmail.com>
2024-02-26 09:04:35 +0800

    Added sample values in user, admin, employee accounts, and sample for bookings

--- database.sql
+++ database.sql
@@ -43,3 +43,18 @@ CREATE TABLE bookings(
 );
 
 DROP TABLE bookings;
+
+--Sample Data for Admin and Employee
+INSERT INTO `employees` (`id`, `name`, `email`, `password_hash`) VALUES
+(1, 'employee1', 'employee1@email.com', '$2y$10$wNHlBY2GXqxhbqzhCOWQNe0Lx0occFnnwzNiYRYoQmjlh16PyvSwa'),
+(2, 'admin1', 'admin1@email.com', '$2y$10$66nmi0OFLO8b5XayGEFISuzjFu0Fd8X51ddoMFgTs./eXDRfU.Hvy');
+
+--Sample Data for User
+INSERT INTO `user` (`id`, `name`, `email`, `password_hash`) VALUES
+(1, 'customer1', 'customer1@email.com', '$2y$10$J97mD.7XAymS0DqpsxF3V.hY5lXtdU/ozZprImnRQ3NR6T1DFBzwq');
+
+--Sample Data for Bookings
+INSERT INTO `bookings` (`id`, `fullName`, `fullAddress`, `email`, `contactNum`, `airconType`, `serviceType`, `message`) VALUES
+(1, 'Lawrence', 'calamba city', 'lawrence@email.com', '9876543210', 'Window Aircon', 'Cleaning', 'CharitooAire Air Conditioning');
+
+
