fb943f913e74a9632796a60452c5795cabbe8bcf
mordredss <lawrencemilitante5@gmail.com>
2024-03-03 10:16:43 +0800

    changed reference db for build_calendar and rename the value in line 11

--- php/bookings.php
+++ php/bookings.php
@@ -1,6 +1,6 @@
 <?php
 function build_calendar($month, $year) {
-    $mysqli = new mysqli('localhost', 'root', '', 'calendar_db');
+    $mysqli = new mysqli('localhost', 'root', '', 'book_db');
     $stmt = $mysqli->prepare("select * from bookings where MONTH(date) = ? AND YEAR(date) = ?");
     $stmt->bind_param('ss', $month, $year);
     $bookings = array();
@@ -8,7 +8,7 @@ function build_calendar($month, $year) {
         $result = $stmt->get_result();
         if($result->num_rows>0){
             while($row = $result->fetch_assoc()){
-                $bookings[] = $row['date'];
+                $bookings[] = $row['Date'];
             }
             
             $stmt->close();
